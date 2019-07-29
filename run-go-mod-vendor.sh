c1f5dcb590201e470e8216933ab521ed51ad1a9d
Donald Hoelle <dhoelle@gmail.com>
2019-07-29 12:56:20 -0700

    install gobin (using go get) if the caller does not have it

--- run-go-mod-vendor.sh
+++ run-go-mod-vendor.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-exec go mod vendor
+go mod vendor
--- run-golangci-lint.sh
+++ run-golangci-lint.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+#
+# This script runs golangci-lint, assuming that it is vendored
+# using go modules and github.com/myitcv/gobin
+
+# if `gobin` is not installed, install it
+# (see: http://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script)
+command -v gobin >/dev/null 2>&1 || { echo >&2 "I require gobin but it's not installed; installing..."; GO111MODULE=off go get -u github.com/myitcv/gobin ; }
+
 exec gobin -m -run github.com/golangci/golangci-lint/cmd/golangci-lint run "$@"
